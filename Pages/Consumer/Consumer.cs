d27a2f58a362e5b410869be7233917a4ed031873
Daniel Gail <daniel.gail@gmail.com>
2019-07-13 00:04:08 -0500

    Added demo test for Consumer web V3

---
+++ Pages/Consumer/Consumer.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+
+namespace AutomationFramework.Pages.Consumer
+{
+    public class Consumer
+    {
+        public class Homepage
+        {
+            public static string homePageURL = "https://v3" + ConfigurationManager.AppSettings.Get("Env") + "consumerweb.rumbleon.com/";
+            public static string homePageTitle = "Welcome to RumbleOn Vehicle Marketplace";
+        }
+    }
+}
---
+++ Tests/Consumer Tests/ConsumerDemo.cs
@@ -0,0 +1,20 @@
+﻿using AutomationFramework.Pages.Consumer;
+using NUnit.Framework;
+using Selenium;
+
+namespace AutomationFramework.Tests
+{
+    class ConsumerDemo : FrameworkCore
+    {
+        [TestCase(Category = "Consumer (Chrome)", TestName = "Validate Homepage")]
+        public void homepageChrome()
+        {
+            SeleniumCommands.SetBrowser("chrome");
+            SeleniumCommands.maximizeBrowser();
+            driver.Url = Consumer.Homepage.homePageURL;
+
+            Assert.AreEqual(driver.Url, Consumer.Homepage.homePageURL);
+            Assert.AreEqual(driver.Title, Consumer.Homepage.homePageTitle);
+        }
+    }
+}
