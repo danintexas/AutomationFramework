2f31eca8ba18fe957f928a29667b108e18de15c6
nusratnmimu <39379263+nusratnmimu@users.noreply.github.com>
2019-07-26 16:16:42 -0500

    Used Category for the class instead of Test cases

--- Tests/Demo Tests/BrowserSupportDemo.cs
+++ Tests/Demo Tests/BrowserSupportDemo.cs
@@ -3,11 +3,13 @@
     using NUnit.Framework;
     using Pages.Google;
 
+    [Category("Multiple Browser Demo")]
     class BrowserSupportDemo : Core
     {
-        [TestCase(Chrome, Category = "Multiple Browser Demo", TestName = "Chrome Test")]
-        [TestCase(Edge, Category = "Multiple Browser Demo", TestName = "Edge Test")]
-        [TestCase(Firefox, Category = "Multiple Browser Demo", TestName = "Firefox Test")]
+        [TestCase(Chrome, TestName = "Chrome Test")]
+        [TestCase(Edge, TestName = "Edge Test")]
+        [TestCase(Firefox, TestName = "Firefox Test")]
+       
         public void MultipleBrowsers(string browser)
         {
             UseBrowser(browser);
