1bcc7269303f8b91754a27a0090b172937b595f8
Daniel Gail <daniel.gail@gmail.com>
2019-08-04 09:38:23 -0500

    Added Delete test to remove listing created in CLS-28

---
+++ Tests/RumbleOn/Classifieds/Smoke Test/Delete Listing.cs
@@ -0,0 +1,20 @@
+﻿namespace AutomationFramework.Tests.Classifieds.Smoke
+{
+    using NUnit.Framework;
+    partial class SmokeTest : Core
+    {
+        [TestCase(Chrome, TestName = "Delete Listing")]
+        [Order(4)]
+        public void Delete(string browser)
+        {
+            WaitForElement(CSS, JsonCall("RumbleOnClassifieds:MyGarage:DeleteButton"));
+            ScreenShot("My Garage Before Delete - " + browser);
+            ClickElement(CSS, JsonCall("RumbleOnClassifieds:MyGarage:DeleteButton"));
+            WaitForElement(CSS, JsonCall("RumbleOnClassifieds:MyGarage:ConfirmDeleteListing"));
+            ClickElement(CSS, JsonCall("RumbleOnClassifieds:MyGarage:ConfirmDeleteListing"));
+            Wait(1);
+            ScreenShot("My Garage After Delete - " + browser);
+
+        }
+    }
+}
