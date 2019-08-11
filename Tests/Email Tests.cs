2ea015adc8934b8d30862223d0af745115098d27
Daniel Gail <daniel.gail@gmail.com>
2019-08-11 13:16:27 -0500

    Daily checkin

---
+++ Tests/Email Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+namespace AutomationFramework.Tests.Development_Tests
+{
+
+    [Category("Dev Tests")]
+    partial class SmokeTest : Core
+    {
+        [TestCase(TestName = "Check Email")]
+        [Order(99)]
+        
+        public void Test()
+        {
+           GetAllEmailsFromAnEmailAccount();
+        }
+       
+    }
+}
