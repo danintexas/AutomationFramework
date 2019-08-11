76722586f5f7cbffc5ff2bb3de9d7952de709190
Daniel Gail <daniel.gail@gmail.com>
2019-08-11 13:26:10 -0500

    Added FetchAllMessages module

--- Core/AdditionalFunctions.cs
+++ Core/AdditionalFunctions.cs
@@ -1,5 +1,8 @@
 ﻿using AutomationFramework.Tests;
+using OpenPop.Mime;
+using OpenPop.Pop3;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 
@@ -46,5 +49,49 @@ class AdditionalFunctions
         
         System.Windows.Forms.SendKeys.SendWait($"{homeDirectory}\\Support\\Image Bank\\" + trueFilename);
         System.Windows.Forms.SendKeys.SendWait("{ENTER}");
+    }
+
+    public static List<Message> FetchAllMessages(string hostname, int port, bool useSsl, string username, string password)
+    {
+        // The client disconnects from the server when being disposed
+        using (Pop3Client client = new Pop3Client())
+        {
+            // Connect to the server
+            client.Connect(hostname, port, useSsl);
+
+            // Authenticate ourselves towards the server
+            client.Authenticate(username, password);
+
+            // Get the number of messages in the inbox
+            int messageCount = client.GetMessageCount();
+
+            // We want to download all messages
+            List<Message> allMessages = new List<Message>(messageCount);
+
+            // Messages are numbered in the interval: [1, messageCount]
+            // Ergo: message numbers are 1-based.
+            // Most servers give the latest message the highest number
+            for (int i = messageCount; i > 0; i--)
+            {
+                allMessages.Add(client.GetMessage(i));
+            }
+
+            int number = 1;
+
+            // Now return the fetched messages
+            foreach (var emailText in allMessages)
+            {
+                MessagePart plainText = emailText.FindFirstPlainTextVersion();
+                var body = plainText.GetBodyAsText();
+                Console.WriteLine(body);
+                plainText.Save(new FileInfo($"c:\\Automation Logs\\Email {number}.txt"));
+                number++;
+            }
+
+            // Delete all messages from the account
+            client.DeleteAllMessages();
+
+            return allMessages;
+        }
     }
 }
