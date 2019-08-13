ebb617f37048a5977e1ebd5376716dcae753262c
Daniel Gail <daniel.gail@gmail.com>
2019-08-13 16:16:20 -0500

    DB test created

---
+++ Tests/Database.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+namespace AutomationFramework.Tests.Development_Tests
+{
+    [Category("Dev Tests")]
+    partial class SmokeTest : Core
+    {
+        [TestCase(TestName = "Check Database")]
+        [Order(99)]
+
+        public void Test()
+        {
+            
+        }
+    }
+}
