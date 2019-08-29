f81644177d0e063f43dab8365ca125d05d6f2c6f
Daniel Gail <daniel.gail@gmail.com>
2019-08-29 12:02:23 -0500

    Locators for V3 Classifieds as well as Page Validation test

---
+++ Tests/RumbleOn/Classifieds V3/1_Validate_Homepage.cs
@@ -0,0 +1,28 @@
+﻿namespace AutomationFramework.Tests.Classifieds.V3_Smoke.Page_Validation
+{
+    using NUnit.Framework;
+
+    [Category("Classifieds Smoketest")]
+    partial class ClassifiedsPageValidation : Core
+    {
+        [TestCase(TestName = "Classifieds Page Validation")]
+        [Order(1)]
+        [Repeat(1)]
+        public void PageValidation()
+        {
+            UseBrowser(JsonCall("FrameworkConfiguration:Browser"));
+            MaximizeBrowser();
+            Logger(Info, "Starting valadiation test of Classifieds Pages");
+            SetUrl = JsonCall("RumbleOnClassifieds:Url:Homepage");
+            Wait(1);
+            ScreenShot("Classifieds Homepage");
+            ShouldBe(SetUrl, JsonCall("RumbleOnClassifieds:Url:Homepage"));
+            ShouldBe(Title, JsonCall("RumbleOnClassifieds:Homepage:Title"));
+            SetUrl = JsonCall("RumbleOnClassifieds:Url:ViewListings");
+            Wait(1);
+            ScreenShot("Classifieds ViewListings Page");
+            ShouldBe(SetUrl, JsonCall("RumbleOnClassifieds:Url:ViewListings"));
+            ShouldBe(Title, JsonCall("RumbleOnClassifieds:Homepage:Title"));
+        }
+    }
+}
